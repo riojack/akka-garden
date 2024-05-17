055617fe56f229cf246de70894b541a270433dcb
riojack <riojack@users.noreply.github.com>
2024-05-16 20:14:48 -0500

    initial commit

---
+++ src/test/java/com/riojack/garden/AppTest.java
@@ -0,0 +1,12 @@
+package com.riojack.garden;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+  @Test
+  void testApp() {
+    assertTrue(true);
+  }
+}
